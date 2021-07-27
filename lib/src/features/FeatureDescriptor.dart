492cc06d105ca1014453ed1268e54d1dcee9c188
Peter Ullrich <dotup.software@gmail.com>
2021-07-27 14:27:51 +0200

    Fix: findFeature does not find top level features

--- lib/src/features/FeatureDescriptor.dart
+++ lib/src/features/FeatureDescriptor.dart
@@ -1,26 +1,29 @@
 import 'package:dotup_flutter_license/dotup_flutter_license.dart';
 import 'package:dotup_flutter_extensions/dotup_flutter_extensions.dart';
 
-class FeatureDescriptor< TFeature> {
+class FeatureDescriptor<TFeature> {
   final TFeature key;
   final LicenseDescriptor license;
   final String name;
   final String description;
-  late final List<FeatureDescriptor< TFeature>> features;
+  late final List<FeatureDescriptor<TFeature>> features;
 
   FeatureDescriptor({
     required this.key,
     required this.license,
     required this.name,
     required this.description,
-    List<FeatureDescriptor< TFeature>>? features,
+    List<FeatureDescriptor<TFeature>>? features,
   }) {
     this.features = features ?? [];
   }
 
-  FeatureDescriptor< TFeature> findFeature(TFeature key) {
-    FeatureDescriptor< TFeature>? find(
-        TFeature key, List<FeatureDescriptor< TFeature>> featuresList) {
+  FeatureDescriptor<TFeature> findFeature(TFeature key) {
+    if (this.key == key) {
+      return this;
+    }
+
+    FeatureDescriptor<TFeature>? find(TFeature key, List<FeatureDescriptor<TFeature>> featuresList) {
       var result = featuresList.firstWhereOrNull((element) => element.key == key);
 
       if (result == null) {

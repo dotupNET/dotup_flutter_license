6c0a5e2230183437368098d1a69a20f41b2a67aa
Peter Ullrich <dotup.software@gmail.com>
2021-08-02 09:36:53 +0200

    LicenseDescriptor operators.

--- lib/src/LicenseDescriptor.dart
+++ lib/src/LicenseDescriptor.dart
@@ -8,4 +8,32 @@ class LicenseDescriptor<T> {
   final String label;
   final Color color;
   LicenseDescriptor({required this.key, required this.index, required this.label, required this.color});
+
+  @override
+  bool operator ==(Object? other) {
+    return other is LicenseDescriptor<T> && this.index == other.index;
+  }
+
+  @override
+  int get hashCode => label.hashCode;
+
+  /// Whether [other] index is numerically smaller than this number.
+  ///
+  /// If either operand is the [double] NaN, the result is always false.
+  bool operator <(LicenseDescriptor<T> other) => index < other.index;
+
+  /// Whether [other] index is numerically smaller than or equal to this number.
+  ///
+  /// If either operand is the [double] NaN, the result is always false.
+  bool operator <=(LicenseDescriptor<T> other) => index <= other.index;
+
+  /// Whether [other] index is numerically greater than this number.
+  ///
+  /// If either operand is the [double] NaN, the result is always false.
+  bool operator >(LicenseDescriptor<T> other) => index > other.index;
+
+  /// Whether [other] index is numerically greater than or equal to this number.
+  ///
+  /// If either operand is the [double] NaN, the result is always false.
+  bool operator >=(LicenseDescriptor<T> other) => index >= other.index;
 }
